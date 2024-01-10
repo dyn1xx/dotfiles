b87b925621fe9135628bbfa0ea9668133d3176f2
dyn1xx <dyn1x@protonmail.com>
2024-01-10 02:05:50 +0200

    Initial commit

---
+++ .config/nvim/lua/v/plugins/zen-mode.lua
@@ -0,0 +1,24 @@
+require("zen-mode").setup({
+    window = {
+        width = 80,
+        height = .85,
+        options = {
+            number = false, -- disable number column
+            relativenumber = false, -- disable number column
+        }
+    },
+
+    -- actions to execute when the Zen window opens
+    on_open = function()
+        vim.opt.linebreak = true
+        vim.opt.wrap = true
+        vim.opt.spell = true
+    end,
+    -- actions to execute when the Zen window closes
+    on_close = function()
+        vim.opt.linebreak = false
+        vim.opt.wrap = false
+        vim.opt.spell = false
+    end,
+
+})
