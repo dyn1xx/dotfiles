1a9382d24d81418f76d5cb45e668dc5d5dd43771
dyn1xx <dyn1x@protonmail.com>
2024-06-24 19:41:48 +0300

    fix mason

--- .config/nvim/lua/v/plugins/lsp-zero.lua
+++ .config/nvim/lua/v/plugins/lsp-zero.lua
@@ -6,7 +6,20 @@ lsp.on_attach(function(client, bufnr)
   lsp.default_keymaps({buffer = bufnr})
 end)
 
--- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+    ensure_installed = {
+        'jdtls',
+        'marksman',
+        'lua-language-server'
+    },
+    handlers = {
+        function(server_name)
+            require('lspconfig')[server_name].setup({})
+        end,
+    },
+})
 
 lsp.setup()
