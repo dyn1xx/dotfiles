97b68a9cd8541610bc6bf2efa062ff7ea252e50b
dyn1xx <dyn1x@protonmail.com>
2024-10-28 23:02:52 +0200

    stuff

--- .config/nvim/lua/v/keybinds.lua
+++ .config/nvim/lua/v/keybinds.lua
@@ -1,9 +1,6 @@
 -- leader key
 vim.g.mapleader = " "
 
--- netrw
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
 -- yank to system clipboard
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
