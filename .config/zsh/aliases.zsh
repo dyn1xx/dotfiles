3c55ff297f94e7d54fb64df384c9ae20eddba6d5
dyn1xx <dyn1x@protonmail.com>
2025-07-22 00:27:18 +0300

    tray

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -8,7 +8,7 @@ alias upg='yay -Suy'
 alias lf='lfcd'
 alias wget='wget --hsts-file="$XDG_CACHE_HOME/wget-hsts"'
 alias yarn='yarn --use-yarnrc "$XDG_CONFIG_HOME/yarn/config"'
-alias song='yt-dlp -x --audio-format mp3 -o "~/media/music/%(channel)s - %(title)s.%(ext)s" --embed-thumbnail --cookies-from-browser firefox'
+alias song='yt-dlp -x --audio-format mp3 -o "$HOME/media/music/%(channel)s - %(title)s.%(ext)s" --embed-thumbnail --embed-metadata'
 alias calc='qalc'
 alias backup='rsync -avh --info=progress2 media personal school notes /mnt/nas'
 alias fix_hyprlock="hyprctl --instance 0 'keyword misc:allow_session_lock_restore 1' && hyprctl --instance 0 'dispatch exec hyprlock'"
