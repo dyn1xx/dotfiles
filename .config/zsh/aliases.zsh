9cb316e9663aa31411c8c32852495f0135c810dc
dyn1xx <dyn1x@protonmail.com>
2025-12-08 03:23:04 +0200

    ff

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -8,7 +8,7 @@ alias v='nvim'
 alias upg='sudo pacman -Suy && yay -Suy'
 alias wget='wget --hsts-file="$XDG_CACHE_HOME/wget-hsts"'
 alias yarn='yarn --use-yarnrc "$XDG_CONFIG_HOME/yarn/config"'
-alias song='yt-dlp -x --audio-format mp3 -o "$HOME/media/music/%(channel)s - %(title)s.%(ext)s" --embed-thumbnail --embed-metadata --extractor-args "youtube:player-client=default,-tv_simply" --cookies-from-browser chromium'
+alias song='yt-dlp -x --audio-format mp3 -o "$HOME/media/music/%(channel)s - %(title)s.%(ext)s" --embed-thumbnail --embed-metadata --extractor-args "youtube:player-client=default,-tv_simply" --cookies-from-browser firefox'
 alias calc='qalc'
 alias backup='rsync -avh --info=progress2 media personal school work notes /mnt/nas'
 alias fix_hyprlock="hyprctl --instance 0 'keyword misc:allow_session_lock_restore 1' && hyprctl --instance 0 'dispatch exec hyprlock'"
