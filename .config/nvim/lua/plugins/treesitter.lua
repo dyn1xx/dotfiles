1678f7c6327b4502d217ba4be97672a90a44ca25
dyn1xx <dyn1x@protonmail.com>
2026-05-01 02:05:07 +0300

    stuff

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,48 +1,48 @@
-return {
-
-    {
-        "nvim-treesitter/nvim-treesitter",
-        branch = "master",
-        build = ":TSUpdate",
-        lazy = false,
-        config = function()
-            require("nvim-treesitter.configs").setup {
-                indent = { enable = true },
-                auto_install = true,
-                incremental_selection = {
-                    enable = true,
-                },
-                highlight = {
-                    enable = true,
-                },
-
-                ensure_installed = {
-                    "bash",
-                    "c",
-                    "diff",
-                    "html",
-                    "javascript",
-                    "jsdoc",
-                    "json",
-                    "jsonc",
-                    "lua",
-                    "luadoc",
-                    "luap",
-                    "markdown",
-                    "markdown_inline",
-                    "python",
-                    "query",
-                    "regex",
-                    "toml",
-                    "tsx",
-                    "typescript",
-                    "vim",
-                    "vimdoc",
-                    "xml",
-                    "yaml",
-                },
-            }
-        end,
-    },
-
-}
+-- return {
+--
+--     {
+--         "nvim-treesitter/nvim-treesitter",
+--         branch = "master",
+--         build = ":TSUpdate",
+--         lazy = false,
+--         config = function()
+--             require("nvim-treesitter.configs").setup {
+--                 indent = { enable = true },
+--                 auto_install = true,
+--                 incremental_selection = {
+--                     enable = true,
+--                 },
+--                 highlight = {
+--                     enable = true,
+--                 },
+--
+--                 ensure_installed = {
+--                     "bash",
+--                     "c",
+--                     "diff",
+--                     "html",
+--                     "javascript",
+--                     "jsdoc",
+--                     "json",
+--                     "jsonc",
+--                     "lua",
+--                     "luadoc",
+--                     "luap",
+--                     "markdown",
+--                     "markdown_inline",
+--                     "python",
+--                     "query",
+--                     "regex",
+--                     "toml",
+--                     "tsx",
+--                     "typescript",
+--                     "vim",
+--                     "vimdoc",
+--                     "xml",
+--                     "yaml",
+--                 },
+--             }
+--         end,
+--     },
+--
+-- }
