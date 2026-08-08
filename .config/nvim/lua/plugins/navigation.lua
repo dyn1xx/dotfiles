127891321bcdcdcf257daf5b3b30cf086a2d3327
dyn1xx <dyn1x@protonmail.com>
2026-08-08 21:19:59 +0300

    trash

--- .config/nvim/lua/plugins/navigation.lua
+++ .config/nvim/lua/plugins/navigation.lua
@@ -40,6 +40,7 @@ return {
     {
         'stevearc/oil.nvim',
         opts = {
+            delete_to_trash = true,
             view_options = {
                 show_hidden = true
             },
